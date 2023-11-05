5b26b8dde897ea80067c253d652f87afd869188e
ABHAY172007 <104831138+ABHAY172007@users.noreply.github.com>
2023-11-05 15:17:41 +0530

    Add files via upload

---
+++ 178pro-main/main.js
@@ -0,0 +1,40 @@
+let latitude = 22.7868542, longitude = 88.3643296;
+
+// Initializing Mapbox
+mapboxgl.accessToken="pk.eyJ1IjoicG9vamEtMTEiLCJhIjoiY2xrc3BwcmptMDFyNzNubnBxbGxqcjAzMyJ9.hXgOsamHpjJJVXAFAw_fsQ"
+
+
+
+
+//Add Mapbox Geocoder
+var map=new mapboxgl.Map({
+    container:"map",
+    style:"mapbox://styles/mapbox/streets-v11",
+    center:[longitude,latitude],
+    zoom:4
+
+})
+map.addControl
+
+
+
+
+
+
+var img1 = document.querySelector("#amber")
+
+// Create a Amber Palace, Jaipur Marker and add it to the map.
+var marker1=new mapboxgl.Marker({
+    element:imgl
+})
+.setLngLat([75.85133,26])
+
+// Create a  Gateway of India, Mumbai Marker and add it to the map.
+
+// Create a India Gate Marker and add it to the map.
+
+
+// Create a Lotus Temple, Delhi Marker and add it to the map.
+
+
+//Create a Victoria Memorial, Kolkata Marker and add it to the map.
